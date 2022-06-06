5cc41a2c237e740b22b2ed14657436920455cc8a
Victor Chidi <victorblessed9@gmail.com>
2022-06-06 22:22:47 +0000

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@ def no_c(my_string):
 
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_strint[i] == 'C':
-            new_string = new_strinf[:(i-j)] + my_string[(i + 1):]
+            new_string = new_string[:(i-j)] + my_string[(i + 1):]
             j += 1
     return new_string
