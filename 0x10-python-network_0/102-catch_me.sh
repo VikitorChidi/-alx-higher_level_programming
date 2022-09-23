784859ebe050df66217a7dccae3803d687a51e56
Victor <victorblessed9@gmail.com>
2022-09-22 23:53:03 -0500

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me -X PUT "user_id=98" -H "Origin: School"
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:School"
