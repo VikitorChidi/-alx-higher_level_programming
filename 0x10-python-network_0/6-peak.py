51235cb43530ba786e238ac9060995db5e9702e3
Victor <victorblessed9@gmail.com>
2022-09-22 23:49:25 -0500

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
 """
-function that finds a peak in a list of unsorted
-integers
+Finds a peak in a list of unsorted integers
 """
 
 
 def find_peak(list_of_integers):
     """
-    function to find peak
+    finds num that's greater than both left and right
     """
-    l = len(list_of_integers)
-    if l == 0:
-        return
-    half = l // 2
-    if (half == l - 1 or list_of_integers[half] >=
-        list_of_integers[half + 1]) and (half == 0 or list_of_integers[half] >=
-                                         list_of_integers[half - 1]):
-        return (list_of_integers[half])
-    elif half != l - 1 and list_of_integers[half + 1] > list_of_integers[half]:
-        return (find_peak(list_of_integers[half + 1:]))
-    return (find_peak(list_of_integers[:half]))
+    if not list_of_integers:
+        return None
+
+    start = 0
+    end = len(list_of_integers)-1
+
+    if list_of_integers[start] > list_of_integers[start+1]:
+        return list_of_integers[start]
+    if list_of_integers[end] > list_of_integers[end-1]:
+        return list_of_integers[end]
+
+    mid = len(list_of_integers) // 2
+    mid_val = list_of_integers[mid]
+
+    if list_of_integers[mid - 1] < mid_val \
+            and list_of_integers[mid + 1] < mid_val:
+        return mid_val
+    if mid_val < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[start:mid + 1])
+    elif mid_val < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:end + 1])
+    else:
+        return list_of_integers[start]
